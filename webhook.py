2622384be6d102cd35e52ed95b121c6943310b8f
Akhil Vamshi Konam <akhil.vamshi@plane.so>
2024-07-31 16:25:39 +0530

    check

---
+++ webhook.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+
+@app.route("/my_webhook", methods=["POST"])
+def return_response():
+    print(request.json)
+    ## Do something with the request.json data.
+    return Response(status=200)
+
+
+if __name__ == "__main__":
+    app.run()
